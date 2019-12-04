ef941a378962d0609310620c4fc3bc8d37c03f32
Artur <dubwoof@me.com>
2019-12-04 15:15:20 +0100

    2.2 - delelted return

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.css";
 import HelloComponent from "./HelloComponent";
 
-const App = () => {
-  return <HelloComponent />;
-};
+const App = () => <HelloComponent />;
 
 export default App;
