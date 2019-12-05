646df2b11b10b1728b2c199d34dc20c510837aad
Artur <dubwoof@me.com>
2019-12-05 14:33:46 +0100

    6.1 - Books: fetch from api

--- src/container/BookList.js
+++ src/container/BookList.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BookItem } from "../components/BookItem";
 
 export const BookList = () => {
-  const books = [{ title: "A" }, { title: "B" }, { title: "C" }];
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4730/books", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(json => setBooks(json));
+  }, []);
 
   return books.map((item, index) => {
     return <BookItem key={index} item={item} />;
